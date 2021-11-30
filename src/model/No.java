4b81a642581449e097c4d35ce59ca1ada60c078f
Eduardo Maciel Sanchez <54561697+DesolateElf-dev@users.noreply.github.com>
2021-11-30 20:43:10 -0300

    Add files via upload
    first upload

---
+++ src/model/No.java
@@ -0,0 +1,38 @@
+package model;
+
+public class No {
+	
+	private String elemento;
+	private No proximo;
+	private No cauda;
+	
+	public No(String elemento, No proximo) {
+		this.elemento = elemento;
+		this.proximo = proximo;
+	}
+	
+	public String getElemento() {
+		return elemento;
+	}
+	
+	public void setElemento(String elemento) {
+		this.elemento =  elemento;
+	}
+	
+	public No getProximo() {
+		return proximo;
+	}
+	
+	public void setProximo(No proximo) {
+		this.proximo = proximo;
+	}
+	
+	public No getCauda() {
+		return cauda;
+	}
+	
+	public void setCauda(No cauda) {
+		this.cauda =  cauda;
+	}
+
+}
