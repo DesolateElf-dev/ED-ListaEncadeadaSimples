81d9fcdd11eb1c7029261ba409ca35d9eeed9fc3
DesolateElf-dev <54561697+DesolateElf-dev@users.noreply.github.com>
2021-11-30 20:58:53 -0300

    exercício 2

--- src/model/ListaEncadeadaSimples.java
+++ src/model/ListaEncadeadaSimples.java
@@ -10,5 +10,14 @@ public class ListaEncadeadaSimples {
 	public void verificaInteiros(No no) {
 		
 	}
+	
+	public void buscaIterativa(No no) {
+		
+	}
+	
+	public void buscaRecurssiva(No no) {
+		
+	}
+	
 
 }
